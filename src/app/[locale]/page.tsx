245b9dc54895fb467c9709728da33aa92534d62d
THEVENARD Julien <thevenard.julien@gmail.com>
2024-09-05 02:09:25 +0200

    setup next-intl

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,13 @@
+import { useTranslations } from "next-intl";
+
 export default function Home() {
+
+  const t = useTranslations("HomePage");
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start font-[family-name:var(--font-geist-sans)]">
-        <h1 className="text-center text-5xl font-bold">6Pool Studios</h1>
+        <h1 className="text-center text-5xl font-bold">{t("welcome")}</h1>
       </main>
     </div>
   );
