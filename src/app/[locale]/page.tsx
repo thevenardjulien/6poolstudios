2a11dc6b14bc18521f4c3edd356bbef4031e1c17
THEVENARD Julien <thevenard.julien@gmail.com>
2024-09-05 03:12:07 +0200

    LanguageChanger component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,20 +1,10 @@
-import { useTranslations } from "next-intl";
+import Welcome from "@/components/Welcome";
 
 export default function Home() {
-
-  const t = useTranslations("HomePage");
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-2 row-start-2 items-center justify-center sm:items-start font-[family-name:var(--font-geist-sans)]">
-        <div className="flex flex-col gap-4 items-center justify-center">
-        <h1 className="flex items-center gap-2 text-center text-5xl font-bold bg-indigo-800 text-white p-4 rounded">
-              {t("welcome")}
-              <br />
-              {t("brand")}
-              </h1>
-            <h2 className="text-center text-2xl">{t("catchphrase")}</h2>
-        </div>
+        <Welcome />
       </main>
     </div>
   );
