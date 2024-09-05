7ef44c89f0a7e11087776c863fa7354e83fc2d79
THEVENARD Julien <thevenard.julien@gmail.com>
2024-09-05 03:57:54 +0200

    create about & contact

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { useTranslations } from "next-intl";
 import LanguageChanger from "./LanguageChanger";
+import Link from "next/link";
 
 export default function Header() {
 
@@ -9,12 +10,12 @@ export default function Header() {
     <header className="flex items-center justify-between gap-4 p-4 bg-main text-white w-full">
       <p className="text-2xl font-bold">6Pool Studios</p>
       <div className="flex items-center gap-4">
-        <a href="/about" className="text-xl hover:underline">
+        <Link href="/about" className="text-xl hover:underline">
           {t("about")}
-        </a>
-        <a href="/contact" className="text-xl hover:underline">
+        </Link>
+        <Link href="/contact" className="text-xl hover:underline">
           {t("contact")}
-        </a>
+        </Link>
         <LanguageChanger />
       </div>
     </header>
